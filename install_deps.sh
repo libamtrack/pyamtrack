9067314334f61a181893a09ecc3998672f38bf34
Leszek Grzanka <grzanka@agh.edu.pl>
2016-06-08 12:06:42 +0200

    fixes

--- install_deps.sh
+++ install_deps.sh
@@ -20,7 +20,8 @@ libamtrack_compile() {
     cd ../
     ls $HOME/usr/lib/
     mkdir -p pyamtrack/libs/
-    cp $HOME/usr/lib/libamtrack.* pyamtrack/libs/
+    cp $HOME/usr/lib/libamtrack*so pyamtrack/libs/
+    cp $HOME/usr/lib/libamtrack*dylib pyamtrack/libs/
 }
 
 apt_install() {
