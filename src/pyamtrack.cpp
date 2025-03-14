d112fd311ec218ede57f780c333dd4da99996aa6
Leszek Grzanka <leszek.grzanka@gmail.com>
2025-03-14 23:33:04 +0100

    Init code (#7)
    * first version
    
    * fixes
    
    * docs

---
+++ src/pyamtrack.cpp
@@ -0,0 +1,14 @@
+#include <pybind11/pybind11.h>
+#include "AT_Constants.h"  // Example libamtrack header
+
+namespace py = pybind11;
+
+// Example wrapper function
+double example_function(double x) {
+    return x * 2.0;  // Replace with an actual libamtrack function
+}
+
+PYBIND11_MODULE(pyamtrack, m) {
+    m.doc() = "Python bindings for libamtrack";
+    m.def("example_function", &example_function, "An example function");
+}
