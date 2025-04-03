85a2e4a7b46f8702511279f6d4f3221444658a7d
Leszek Grzanka <leszek.grzanka@gmail.com>
2025-04-03 22:59:53 +0200

    Refactor beta_from_energy and energy_from_beta functions to directly use AT_PhysicsRoutines for improved clarity and maintainability

--- src/converters/beta_from_energy.cpp
+++ src/converters/beta_from_energy.cpp
@@ -1,22 +1,10 @@
 #include "../wrapper_template.h"
 #include "beta_from_energy.h"
-#include <pybind11/numpy.h>
-#include <pybind11/stl.h>
-#include <cstddef>
-
-#define PYBIND11_DETAILED_ERROR_MESSAGES
-
 
 extern "C" {
     #include "AT_PhysicsRoutines.h"
 }
 
-namespace py = pybind11;
-
-double beta_from_energy_single(double E_MeV_u) {
-    return AT_beta_from_E_single(E_MeV_u);
-}
-
 py::object beta_from_energy(py::object input) {
-    return wrap_function(beta_from_energy_single, input);
+    return wrap_function(AT_beta_from_E_single, input);
 }
