b81bf57405831dddbaaa1ef02a94873e24ddf7ef
Leszek Grzanka <leszek.grzanka@gmail.com>
2025-04-07 16:48:43 +0200

    Refactor converters and stopping modules: consolidate functionality and update bindings

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,4 @@
 #include <pybind11/pybind11.h>
-#include "converters/beta_from_energy.h"
-#include "converters/energy_from_beta.h"
-#include "stopping/electron_range.h"
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
@@ -10,38 +7,6 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(_core, m) {
     m.doc() = "Python bindings for libamtrack";
-
-    m.def(
-        "beta_from_energy",
-        &beta_from_energy,
-        py::arg("input"),
-        py::return_value_policy::automatic,
-        beta_from_energy_doc
-    );
-
-    m.def(
-        "energy_from_beta",
-        &energy_from_beta,
-        py::arg("input"),
-        py::return_value_policy::automatic,
-        energy_from_beta_doc
-    );
-
-    m.def(
-        "electron_range",
-        &electron_range,
-        py::arg("input"),
-        py::return_value_policy::automatic,
-        R"pbdoc(
-        Calculate electron range (m).
-
-        Parameters:
-            input (float | int | numpy.ndarray | list): The energy per nucleon in MeV/u. Can be a single value, a NumPy array, or a Python list.
-
-        Returns:
-            float | numpy.ndarray | list: The calculated electron range(s). Returns a float for a single input, a NumPy array for a NumPy array input, or a Python list for a list input.
-        )pbdoc"
-    );
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
--- src/pyamtrack/__init__.py
+++ src/pyamtrack/__init__.py
@@ -34,8 +34,5 @@ if sys.platform == "win32":
         except OSError as e:
             print(f"Warning: failed to load {dll_name} from {dll_path}: {e}")
 
-from ._core import electron_range
-from . import converters
 
-__all__ = ["__doc__", "__version__", "electron_range"]
-__all__.append("converters")
+__all__ = ["__doc__", "__version__"]
