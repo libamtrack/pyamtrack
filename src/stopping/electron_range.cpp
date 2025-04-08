bbc8e0ed07ac2970272359e9615836051c4c36fe
Leszek Grzanka <leszek.grzanka@gmail.com>
2025-04-08 16:50:51 +0200

    Refactor electron_range function to improve material type checking and error handling

--- src/stopping/electron_range.cpp
+++ src/stopping/electron_range.cpp
@@ -17,11 +17,19 @@ py::object electron_range(py::object input, py::object material = py::int_(1)) {
     if (py::isinstance<py::int_>(material)) {
         material_id = material.cast<int>();
     } else {
-        // Assuming Material class has a get_id() method
+        // Check if material is an instance of Material class
+        py::module_ builtins = py::module_::import("builtins");
+        py::object type_func = builtins.attr("type");
+        py::str type_name = type_func(material).attr("__name__").cast<py::str>();
+        
+        if (std::string(type_name) != "Material") {
+            throw py::type_error("Material argument must be either an integer or a Material object");
+        }
+        
         try {
             material_id = material.attr("id").cast<int>();
         } catch (const py::error_already_set&) {
-            throw py::type_error("Material argument must be either an integer or a Material object with 'id' attribute");
+            throw py::type_error("Material object must have 'id' attribute");
         }
     }
     return wrap_function(electron_range_single, input);
