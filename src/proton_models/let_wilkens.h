87fda72010f614104ed8f470950751e3af71a033
witNie <witold.niec@gmail.com>
2026-06-05 14:45:00 +0200

    Implement Wilkens model for LET dose calculation and update CMake targets

--- src/proton_models/let_wilkens.h
+++ src/proton_models/let_wilkens.h
@@ -1,5 +1,5 @@
-#ifndef ELECTRON_RANGE_H
-#define ELECTRON_RANGE_H
+#ifndef LET_WILKENS_H
+#define LET_WILKENS_H
 
 
 #include <map>
@@ -8,3 +8,12 @@
 #include "../wrapper/multi_argument.h"
 #include "../particles/particles.h"
 #include "../particles/ions/ion.h"
+
+
+
+// nb::object let_track(nb::object depth_cm, long material, double energy_MeV, double energy_spread_MeV);
+
+
+nb::object let_dose(nb::object depth_cm, long material, double energy_MeV, double energy_spread_MeV);
+
+#endif  // LET_WILKENS_H
